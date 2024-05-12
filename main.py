00b86820b7d9ddde30b98c29cb982b8aca8f6fea
SolomonAHailu <solomonasregdew1992@gmail.com>
2024-05-12 06:58:02 -0900

    some typo fix

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import motor_controller as mc
 def main():
     try:
         mc.setup()
-        # mc.set_motor_speed1(40)
-        # mc.set_motor_speed2(80)
-        # mc.forward(1)
-        # mc.reverse(1)
-        # mc.left(1)
+        mc.forward(5)
+        mc.reverse(5)
+        mc.left(1)
         mc.right(1, speed1 = 40)
     except Exception as e:
         print(f"An error occurred: {e}")
